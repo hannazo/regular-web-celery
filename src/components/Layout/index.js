87a73f0aa1a0577344d0ad8892804d62b3fefe2f
hannazo <106128732+hannazo@users.noreply.github.com>
2023-05-17 10:31:01 -0400

    added style elements to layout

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,13 +1,25 @@
+import { Outlet } from 'react-router-dom';
 import Navbar from '../Navbar';
 import Footer from '../Footer';
 import './index.scss';
 
 const Layout = () => {
     return (
-        <>
+        <div className='App'>
             <Navbar />
+            <div className='page'>
+                <span className='tags top-tags'>&lt;body&gt;</span>
+
+                <Outlet />
+
+                <span className='tags bottom-tags'>&lt;/body&gt;
+                <br />
+                <span className='bottom-tag-html'>&lt;/html&gt;</span>
+                </span>
+
+            </div>
             <Footer />
-        </>
+        </div>
     )
 }
 
