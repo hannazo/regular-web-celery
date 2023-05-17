410ec8f4f53a973e3b9af2af53e69b1fe701202e
hannazo <106128732+hannazo@users.noreply.github.com>
2023-05-17 16:08:15 -0400

    added home page left side text and button

---
+++ src/components/Home/index.js
@@ -0,0 +1,21 @@
+import { Link } from 'react-router-dom'
+import './index.scss';
+
+const Home = () => {
+    return (
+        <div className="container home-page">
+            <div className="text-zone">
+                <h1>Hi, <br /> I'm
+                Hanna,
+                <br />
+                web developer.
+                </h1>
+                <h2>Frontend Developer / Freelancer</h2>
+                <Link to="contact" className="flat-button">CONTACT ME</Link>
+
+            </div>
+        </div>
+    );
+};
+
+export default Home
