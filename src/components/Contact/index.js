ab94d7379e400d81a2bf3599ea397098422a1fb8
hannazo <106128732+hannazo@users.noreply.github.com>
2023-05-19 16:13:20 -0400

    revised contact page

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -25,30 +25,32 @@ const Contact = () => {
 
     return (
         <div className="container contact-page">
-            <div className="text-zone">
-                <div className="title">
-                    <h1>Be in touch</h1>
-                </div>
-                <div className="contact-form">
-                    <form ref={form} onSubmit={sendEmail}>
-                        <ul>
-                            <li className="half">
-                                <input placeholder="Name" type="text" name="name" required />
-                            </li>
-                            <li className="half">
-                                <input placeholder="Email" type="email" name="email" required />
-                            </li>
-                            <li>
-                                <input placeholder="Subject" type="text" name="subject" required />
-                            </li>
-                            <li>
-                                <textarea placeholder="Message" name="message" required ></textarea>
-                            </li>
-                            <li>
-                                <input type="submit" className="flat-button" value="SEND" />
-                            </li>
-                        </ul>
-                    </form>
+            <div className="content-wrapper">
+                <div className="text-zone">
+                    <div className="title">
+                        <h1>Be in touch</h1>
+                    </div>
+                    <div className="contact-form">
+                        <form ref={form} onSubmit={sendEmail}>
+                            <ul>
+                                <li className="half">
+                                    <input placeholder="Name" type="text" name="name" required />
+                                </li>
+                                <li className="half">
+                                    <input placeholder="Email" type="email" name="email" required />
+                                </li>
+                                <li>
+                                    <input placeholder="Subject" type="text" name="subject" required />
+                                </li>
+                                <li>
+                                    <textarea placeholder="Message" name="message" required ></textarea>
+                                </li>
+                                <li>
+                                    <input type="submit" className="flat-button" value="SEND" />
+                                </li>
+                            </ul>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
