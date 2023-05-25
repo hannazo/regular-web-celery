9687f50d279b7fa6bad20c5b2155b3b7ba724e0d
hannazo <106128732+hannazo@users.noreply.github.com>
2023-05-25 19:49:34 -0400

    added portfolio page

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -40,7 +40,7 @@ const Home = () => {
                             idx={22}
                         />
                     </h1>
-                    <h2>Frontend Developer / Freelancer</h2>
+                    <h2>Full Stack Developer / Freelancer</h2>
                     <Link to="contact" className="flat-button">CONTACT ME</Link>
                 </div>
             </div>
--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,14 +1,40 @@
 import './index.scss';
+import project1 from '../../assets/images/project-1.png';
+import project2 from '../../assets/images/project-2.png';
+import project3 from '../../assets/images/project-3.png';
+import project4 from '../../assets/images/project-4.png';
+import project5 from '../../assets/images/project-5.png';
+import project6 from '../../assets/images/project-6.png';
 
 const Portfolio = () => {
+
+    const projects = [
+        {title: "Book Search Engine", image: project1, link: "https://vast-tundra-70816.herokuapp.com/"},
+        {title: "Feed The Foodie", image: project2, link: "https://sarasg89.github.io/feed-the-foodie/"},
+        {title: "Tech Blog", image: project3, link: "https://dry-journey-24079.herokuapp.com/"},
+        {title: "Weather Dashboard", image: project4, link: "https://hannazo.github.io/weather-or-not/"},
+        {title: "Work Day Scheduler", image: project5, link: "https://hannazo.github.io/stay-busy/"},
+        {title: "Note Taker", image: project6, link: "https://boiling-fjord-26165.herokuapp.com/"}
+    ];
+
     return (
         <div className="container portfolio-page">
-            <div className="text-zone">
-                <div className="title">
-                    <h1>Portfolio</h1>
+            <div className="content-wrapper">
+                <div className="text-zone">
+                    <div className="title">
+                        <h1>Portfolio</h1>
+                    </div>
+                    <div className="projects-grid">
+                    {projects.map((project, index) => (
+                            <a href={project.link} target="_blank" rel="noopener noreferrer" className="project-card" key={index}>
+                                <img src={project.image} alt={project.title} />
+                                <h2>{project.title}</h2>
+                            </a>
+                        ))}
+                    </div>
                 </div>
             </div>
-        </div>
+        </div >
     );
 };
 
